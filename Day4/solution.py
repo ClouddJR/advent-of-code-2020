d18adc5ccdf17277ce04b5e5faa9f3110e473d49
Arkadiusz Chmura <arekchmura@gmail.com>
2020-12-04 13:28:58 +0100

    Make validation more readable for day 4 puzzle

--- Day4/solution.py
+++ Day4/solution.py
@@ -4,24 +4,17 @@ import re
 def isFieldValid(field):
     name = field[0]
     value = field[1]
-    if name == 'byr':
-        return len(value) == 4 and 1920 <= int(value) <= 2002
-    elif name == 'iyr':
-        return len(value) == 4 and 2010 <= int(value) <= 2020
-    elif name == 'eyr':
-        return len(value) == 4 and 2020 <= int(value) <= 2030
-    elif name == 'hgt':
-        return re.match('^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$', value) != None
-    elif name == 'hcl':
-        return re.match('^#[a-f0-9]{6}$', value) != None
-    elif name == 'ecl':
-        return value in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
-    elif name == 'pid':
-        return len(value) == 9
-    elif name == 'cid':
-        return True
-
-    return False
+    validation_rules = {
+        'byr': lambda v: len(v) == 4 and 1920 <= int(v) <= 2002,
+        'iyr': lambda v: len(v) == 4 and 2010 <= int(v) <= 2020,
+        'eyr': lambda v: len(v) == 4 and 2020 <= int(v) <= 2030,
+        'hgt': lambda v: bool(re.match('^((1[5-8][0-9]|19[0-3])cm)|((59|6[0-9]|7[0-6])in)$', v)),
+        'hcl': lambda v: bool(re.match('^#[a-f0-9]{6}$', v)),
+        'ecl': lambda v: v in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'],
+        'pid': lambda v: len(v) == 9,
+        'cid': lambda v: True
+    }
+    return validation_rules[name](value)
 
 
 def isPassportValid(passport):
