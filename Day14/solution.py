e4bb7e42727d534c9604a17d0fc398606cbb65d4
Arkadiusz Chmura <arekchmura@gmail.com>
2020-12-14 09:22:38 +0100

    Add solution for day 14 puzzle

---
+++ Day14/solution.py
@@ -0,0 +1,50 @@
+import re
+
+input = []
+with open('input.txt') as f:
+    input = [line.strip() for line in f]
+
+# part 1
+mask = None
+memory = {}
+
+for instr in input:
+    if instr.startswith('mem'):
+        addr, value = re.findall('mem\[(\d+)\] = (\d+)', instr)[0]
+        masked_value = ''.join([
+            v if m == 'X' else m for m, v in zip(mask, format(int(value), '036b'))
+        ])
+        memory[addr] = ''.join(masked_value)
+    else:
+        mask = re.findall('mask = ([10X]{36})', instr)[0]
+
+print(sum([int(value, 2) for _, value in memory.items()]))
+
+
+# part 2
+mask = None
+memory = {}
+
+for instr in input:
+    if instr.startswith('mem'):
+        addr, value = re.findall('mem\[(\d+)\] = (\d+)', instr)[0]
+
+        masked_addr = ''.join([
+            'X' if m == 'X' else '1' if m == '1' else v for m, v in zip(mask, format(int(addr), '036b'))
+        ])
+
+        all_addreses = [masked_addr]
+        for i in range(mask.count('X')**2):
+            for addr in all_addreses:
+                if 'X' in addr:
+                    next_X = next(i for i, c in enumerate(addr) if c == 'X')
+                    all_addreses.remove(addr)
+                    all_addreses.append(addr[:next_X] + '1' + addr[next_X+1:])
+                    all_addreses.append(addr[:next_X] + '0' + addr[next_X+1:])
+
+        for addr in all_addreses:
+            memory[addr] = format(int(value), '036b')
+    else:
+        mask = re.findall('mask = ([10X]{36})', instr)[0]
+
+print(sum([int(value, 2) for _, value in memory.items()]))
